07ca63836da5b6d01b61681d017990c2204f9ef1
janani811 <jananigopalsanthi@gmail.com>
2023-02-10 23:38:43 +0530

    nest and db setup

---
+++ prisma/db/index.ts
@@ -0,0 +1,30 @@
+import { PrismaClient } from '@prisma/client';
+
+export const prisma = new PrismaClient();
+
+async function main() {
+  //   await prisma.user.create({
+  //     data: {
+  //       name: 'Alice',
+  //       email: 'alice@prisma.io',
+  //       posts: {
+  //         create: { title: 'Hello World' },
+  //       },
+  //     },
+  //   });
+  //   const allUsers = await prisma.user.findMany({
+  //     include: {
+  //       posts: true,
+  //     },
+  //   });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });

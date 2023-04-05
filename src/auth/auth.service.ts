628ccac858c75f97d80eb5092f2fb541963bb313
sachinelavarasan <sachinelavarasan321@gmail.com>
2023-04-05 22:33:20 +0530

    auth flow chnaged

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,12 @@
 /* eslint-disable prettier/prettier */
-import { Injectable, ForbiddenException } from '@nestjs/common';
+import { Injectable, ForbiddenException, HttpException, HttpStatus } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
+import { Knex } from 'knex';
+import { InjectModel } from 'nest-knexjs';
 import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { SignInDto, SignUpDto } from './auth.dto';
-import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
 @Injectable()
 export class AuthService {
@@ -13,131 +14,121 @@ export class AuthService {
     private prisma: PrismaService,
     private jwtService: JwtService,
     private config: ConfigService,
+    @InjectModel() private readonly knex: Knex,
   ) {}
 
   async fetchProfile(id: number) {
-    const user: any = await this.prisma.orgUsers.findUnique({
-      where: {
-        oga_id: id,
-      },
-      include: {
-        organisation: true,
-      },
-    });
-    // if user does not exist throw exception
-    if (!user) {
-      throw new ForbiddenException('User not found');
+    try {
+      const user: any = await this.knex
+        .table('users')
+        .join('organisations', 'org_id', 'us_org_id')
+        .where('us_id', id)
+        .column('*')
+        .first();
+      if (!user) {
+        throw new ForbiddenException('User not found');
+      }
+      delete user.us_password;
+      return { user };
+    } catch (e) {
+      console.log(e);
     }
-
-    delete user.password;
-    return { user };
   }
 
   async signup(dto: SignUpDto) {
-    const user: any = await this.prisma.orgUsers.findUnique({
-      where: {
-        oga_email: dto.email,
-      },
-    });
-    // if user does not exist throw exception
-    if (user) {
-      throw new ForbiddenException('Organisation email already exists');
+    let org;
+    let newUser;
+
+    const existOrg = await this.knex.table('organisations').where('org_email', dto.email);
+
+    if (existOrg.length) {
+      throw new HttpException('Organisation email already exists', HttpStatus.BAD_REQUEST);
     }
 
-    const hashPass = await bcrypt.hash(dto.password, 10);
-
     try {
-      const organisation: any = await this.prisma.org.create({
-        data: {
-          org_name: dto.orgName,
-          org_address: dto.orgAddress,
-        },
+      org = await this.knex.table('organisations').insert({
+        org_name: dto.orgName,
+        org_address: dto.orgAddress,
+        org_email: dto.email,
+        org_details: '',
+        org_phone_no: '',
       });
-      const user: any = await this.prisma.orgUsers.create({
-        data: {
-          oga_email: dto.email,
-          oga_password: hashPass,
-          oga_name: dto.name,
-          oga_user_type: 1,
-          oga_org_id: organisation.org_id,
-        },
+      console.log(org);
+    } catch (e) {
+      throw new ForbiddenException('Something went wrong');
+    }
+    try {
+      const salt = await bcrypt.genSalt(10);
+      const hashPass = await bcrypt.hash(dto.password, 10);
+      newUser = await this.knex.table('users').insert({
+        us_name: dto.name,
+        us_email: dto.email,
+        us_password: hashPass,
+        us_password_salt: salt,
+        us_org_id: org,
+        us_type: 1,
+        us_verification_token: '',
+        us_address: '',
+        us_phone_no: '',
       });
-
-      delete user.password;
-      return { ...organisation, ...user };
-    } catch (error) {
-      if (error instanceof PrismaClientKnownRequestError) {
-        if (error.code === 'P2002') {
-          throw new ForbiddenException('Credentials taken');
-        }
-      }
-      throw error;
+      delete newUser.us_password;
+      return { ...newUser };
+    } catch (e) {
+      throw new ForbiddenException('Something went wrong');
     }
   }
 
   async signin(dto: SignInDto) {
-    const user: any = await this.prisma.orgUsers.findUnique({
-      where: {
-        oga_email: dto.email,
-      },
-      include: {
-        organisation: true,
-      },
-    });
+    const user: any = await this.knex.table('users').where('us_email', dto.email).first();
+
     // if user does not exist throw exception
     if (!user) {
       throw new ForbiddenException('User not found');
     }
 
     // compare password
-    const isMatch = await bcrypt.compare(dto.password, user.oga_password);
-
+    const isMatch = await bcrypt.compare(dto.password, user.us_password);
+    // console.log(isMatch, user.us_password);
     // if password incorrect throw exception
     if (!isMatch) throw new ForbiddenException('Your Credentials are incorrect');
-    delete user.password;
+    delete user.us_password;
     const jwtToken = await this.signInToken(user);
     return { user, jwtToken };
   }
 
   async signInToken(user): Promise<string> {
     const secret = this.config.get('JWT_SECRET');
-
-    const token = await this.jwtService.signAsync(user, {
-      expiresIn: '8h',
-      secret: secret,
-    });
-
-    return token;
+    try {
+      const token = await this.jwtService.sign(
+        { ...user },
+        {
+          expiresIn: '8h',
+          secret: secret,
+        },
+      );
+      return token;
+    } catch (e) {
+      throw new ForbiddenException('Authentication failed');
+    }
   }
 
   async editProfile(data: any) {
-    await this.prisma.orgUsers.update({
-      where: {
-        oga_id: data?.orgAdminId,
-      },
-      data: {
-        oga_name: data?.name,
-        oga_email: data?.email,
-      },
+    await this.knex.table('users').where({ us_id: data.userId }).update({
+      us_name: data.name,
+      us_email: data.email,
     });
-    await this.prisma.org.update({
-      where: {
-        org_id: data?.orgId,
-      },
-      data: {
-        org_name: data?.orgName,
-        org_address: data?.orgAddress,
-      },
+    await this.knex.table('organisations').where({ org_id: data.orgId }).update({
+      org_name: data.orgName,
+      org_address: data.orgAddress,
+      org_email: data.email,
+      org_phone_no: data.phone,
     });
 
-    const user: any = await this.prisma.orgUsers.findUnique({
-      where: {
-        oga_id: data?.orgAdminId,
-      },
-      include: {
-        organisation: true,
-      },
-    });
+    const [user]: any = await this.knex
+      .table('users')
+      .join('organisations', 'users.us_org_id', '=', 'organisations.org_id')
+      .where('us_id', data.userId)
+      .select('*');
     delete user.password;
     return { user };
   }
